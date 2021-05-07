db9bb9f2e354a12eafd52d6be0f40b775d34603a
AmmarJT <AmmarTashk@gmail.com>
2021-05-07 21:57:37 +0300

    just added a sortable buttons

--- resources/views/ajaxRequest.blade.php
+++ resources/views/ajaxRequest.blade.php
@@ -5,8 +5,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/4.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 </head>
 <body>
@@ -37,6 +42,18 @@
   
         </form>
     </div>
+
+    <div class="sortable">
+        <div data-id='1' class="item child m-2">
+            <a href="" class="btn btn-primary btn-lg btn-block">First</a>
+        </div>
+        <div data-id='2' class="item child m-2">
+            <a href="" class="btn btn-primary btn-lg btn-block">Second</a>
+        </div>
+        <div data-id='3' class="item child m-2">
+            <a href="" class="btn btn-primary btn-lg btn-block">Third</a>
+        </div>
+    </div>
   
 </body>
 <script type="text/javascript">
@@ -56,18 +73,30 @@
         var email = $("input[name=email]").val();
    
         $.ajax({
-           type:'POST',
-           url:"{{ route('ajaxRequest.post') }}",
-           data:{name:name, password:password, email:email},
-           success:function(data){
-              alert(data.success);
-              console.log('name: ' + data.input.name);
-              console.log('password: ' + data.input.password);
-              console.log('email: ' + data.input.email);
-           }
+            type:'POST',
+            url:"{{ route('ajaxRequest.post') }}",
+            data:{
+                name:name, password:password, email:email
+            },
+            success:function(data){
+                alert(data.success);
+                console.log('name: ' + data.input.name);
+                console.log('password: ' + data.input.password);
+                console.log('email: ' + data.input.email);
+            }
         });
   
     });
+
+
+
+    $(document).ready(function () {               
+        $('.sortable').sortable();
+
+        $('.sorty').sortable();
+    });
+
+
 </script>
    
 </html>
